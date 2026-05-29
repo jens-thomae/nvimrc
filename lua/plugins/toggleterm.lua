381aa55758bdcda40b3e51c4249f16ffd87e5ffe
Jens Thomae <jens.thomae@proton.me>
2026-05-29 11:17:33 +0200

    Fix toggleterm, now only using insert mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,6 +5,8 @@ return {
 		require("toggleterm").setup({
 			shade_terminals = true,
 			size = 80,
+			start_in_insert = true,
+			persist_mode = false,
 		})
 
 		local Terminal = require("toggleterm.terminal").Terminal
@@ -15,7 +17,6 @@ return {
 			direction = "float",
 			float_opts = { border = "double" },
 			on_open = function(term)
-				vim.cmd("startinsert!")
 				vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = term.bufnr, noremap = true, silent = true })
 			end,
 			on_close = function() vim.cmd("startinsert!") end,
@@ -25,7 +26,6 @@ return {
 			direction = "float",
 			float_opts = { border = "double" },
 			on_open = function(term)
-				vim.cmd("startinsert!")
 				-- <D-i> for Neovide / Alacritty (kitty keyboard protocol); <M-i> for inside tmux
 				local close_term = [[<C-\><C-n> <bar> <Cmd>close<CR>]]
 				vim.keymap.set("t", "<D-i>", close_term, { buffer = term.bufnr, noremap = true, silent = true })
